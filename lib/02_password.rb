230c373acb60272909de7a34c8a6be0b620510dd
MuTtonye91 <murielle.akoo@live.fr>
2019-01-10 18:18:03 +0100

    add prog password

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,11 +1,34 @@
 def signup
-
+	puts "Veuillez définir un mot de passe : "
+	print ">"
+	password = gets.chomp
+	return password
 end
 
 def login
-
+	puts "Rentrez votre mot de passe"
+	print ">"
+	pwd = gets.chomp
+	return pwd
 end
 
-def welcome_screen
 
+def welcome_screen (password, pwd)
+	while password != pwd
+		puts "Mot de passe incorrect. ACCES INTERDIT !"
+		print ">"
+		pwd = login
+	end
+	puts "Bienvenue dans votre espace NASA TOP SECRET !"
+		
 end
+
+def perform
+	password = signup
+	pwd = login
+	welcome_screen(password, pwd)
+end
+
+perform
+
+
